70c5b528f547c212d433d5f8375a0bbed7bdce21
poopcaap <143746602+poopcaap@users.noreply.github.com>
2023-12-10 02:39:50 -0500

    Update countdata.py

--- countdata.py
+++ countdata.py
@@ -1,12 +1,9 @@
 import json
-
-# 读取JSON文件
+#this is for counting length of json
 with open('routes.json', encoding='utf-8') as file:
     data = json.load(file)
 
-# 检查数据类型并计算条目数
 if isinstance(data, list):
-    # 如果数据是一个列表，直接计算长度
     count = len(data)
 
 print("Number of data entries:", count)
